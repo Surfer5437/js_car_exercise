2797cbfddad29e674c8793b77909e49f4d9bf811
Brandon <livingstonebrandon5437@gmail.com>
2023-02-22 18:45:34 -0500

    finished part 2 of OOP exercise

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ class Vehicle {
         console.log("Beep.");
     }
 }
+class Car extends Vehicle{
+    constructor(make, model, year){
+        super(make,model,year);
+        this.numWheels = 4;
+    }
+}
