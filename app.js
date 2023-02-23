3c8edbd305bcae04b754fce48441ed5e37d3f0e9
Brandon <livingstonebrandon5437@gmail.com>
2023-02-22 22:31:54 -0500

    finished exercise in OOP

--- app.js
+++ app.js
@@ -13,7 +13,38 @@ class Vehicle {
 }
 class Car extends Vehicle{
     constructor(make, model, year){
+        
         super(make,model,year);
+        if (this.make===undefined || this.model === undefined || this.year === undefined){
+            throw new Error('Only vehicles are allowed here!')
+        }
         this.numWheels = 4;
+    }
+}
+class Motorcycle extends Vehicle{
+    constructor(make, model, year){
+        super(make,model,year);
+        if (this.make===undefined || this.model === undefined || this.year === undefined){
+            throw new Error('Only vehicles are allowed here!')
+        }
+        this.numWheels = 2;
+    }
+}
+class Garage{
+    constructor(vehCap){
+        this.vehCap = vehCap;
+        this.vehicles = [];
+    }
+    add(c){
+        if (!(c instanceof Vehicle)){
+            return "Only vehicles are allowed in here!";
+        }
+        if (this.vehicles.length !== this.vehCap){
+        this.vehicles.push(c);
+        return "Vehicle added!";
+        }
+        else{
+            return "Sorry, we're full.";
+        }
     }
 }
